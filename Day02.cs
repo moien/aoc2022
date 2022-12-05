e96d725e61b329d00cb15c5e1371a2dfb4221c6c
Magnus Öien <Magnus.Oien@gmail.com>
2022-12-05 11:13:23 +0100

    Elves confirmed retarded.

--- Day02.cs
+++ Day02.cs
@@ -1,14 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using IronPython.Modules;
+using static IronPython.Modules.PythonThread;
 
 namespace AoC2022
 {
     internal class Day02
     {
-   
+        public enum Move
+        {
+            Rock = 1,
+            Paper = 2,
+            Scissors = 3,
+        }
 
+        public static Move ConvertWeirdAndStupidSuggestedMove(Move elfMove, string superWeirdMoveCode_dudeWtfAreYouDoing)
+        {
+            switch (superWeirdMoveCode_dudeWtfAreYouDoing)
+            {
+                case "X": return elfMove == Move.Rock ? Move.Scissors : elfMove == Move.Paper ? Move.Rock : Move.Paper;
+                case "Y": return elfMove;
+                case "Z": return elfMove == Move.Rock ? Move.Paper : elfMove == Move.Paper ? Move.Scissors : Move.Rock;
+                default:
+                    return Move.Rock;
+            }
+        }
+        public static Move ConvertWeirdAndStupidElfNotation(string superWeirdMoveCode_dudeWtfAreYouDoing)
+        {
+            switch (superWeirdMoveCode_dudeWtfAreYouDoing)
+            {
+                case "A": case "X":  return Move.Rock;
+                case "B": case "Y": return Move.Paper;
+                case "C": case "Z": return Move.Scissors;
+                default:
+                    return Move.Rock;
+            }
+
+        }
         public static void Run()
         {
+            var lines = File.ReadAllText(@"C:\files\temp\day02input.txt").Split(
+                new string[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.None
+            );
             
+            Int64 totalPoints = 0;
+            foreach (var line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    var moves = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    var elfMove = ConvertWeirdAndStupidElfNotation(moves.First().Trim().ToUpper());
+                    var ownMove = ConvertWeirdAndStupidSuggestedMove(elfMove, moves.Skip(1).First().Trim().ToUpper());
+                    
+                    var winPoints = 
+                        elfMove == Move.Rock && ownMove == Move.Rock ? 3 : elfMove == Move.Rock && ownMove == Move.Paper ? 6 :
+                        elfMove == Move.Paper && ownMove == Move.Paper ? 3 : elfMove == Move.Paper && ownMove == Move.Scissors ? 6 :
+                        elfMove == Move.Scissors && ownMove == Move.Scissors ? 3 : elfMove == Move.Scissors && ownMove == Move.Rock ? 6 : 0;
+
+                    var moveSpecificPoints = (int)ownMove;
+
+                    totalPoints += winPoints + moveSpecificPoints;
+                    
+                }
+
+                
+            }
+
+
+            Console.WriteLine(totalPoints);
         }
     }
 }
---
+++ Day03.cs
@@ -0,0 +1,32 @@
+﻿using IronPython.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static IronPython.Modules._ast;
+using static IronPython.Modules.ArrayModule;
+
+namespace AoC2022
+{
+    internal class Day03
+    {
+        public static void Run()
+        {
+            var lines = File.ReadAllText(@"C:\files\temp\day03input.txt").Split(
+                new string[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.None
+            );
+
+            Dictionary<int, string> ElfIsOnceAgainStupidAndWritesWeirdListsPriorityList = new Dictionary<int, string>();
+            int index = 1;
+            for (char character = 'a'; character <= 'z'; ++character, index++)
+            {
+                ElfIsOnceAgainStupidAndWritesWeirdListsPriorityList.Add(index, character.ToString());
+                ElfIsOnceAgainStupidAndWritesWeirdListsPriorityList.Add(index + 26, character.ToString().ToUpper());
+            }
+
+            Int64 sum = (from line in lines let halfTheItems = line.Length / 2 let firstCompartment = line.Substring(0, halfTheItems).ToList() let secondCompartment = line.Substring(halfTheItems, halfTheItems).ToList() from character in firstCompartment.Where(firstCompartmentItem => secondCompartment.Any(secondCompartmentItem => secondCompartmentItem.Equals(firstCompartmentItem))).Distinct() select ElfIsOnceAgainStupidAndWritesWeirdListsPriorityList.Single(item => item.Value == character.ToString()).Key).Aggregate<int, long>(0, (current, priorityString) => current + priorityString);
+            Console.WriteLine(sum);
+        }
+    }
+}
